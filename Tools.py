1072e817933d87d103aa196657fabbc8bbf162ff
wang <2824023332@qq.com>
2025-02-05 21:07:42 +0800

    1

--- Tools.py
+++ Tools.py
@@ -1,10 +1,7 @@
-import time
 import json
-import random
 import mss
 import mss.tools
-import pyautogui
-import keyboard
+import cv2
 
 def capture_screen(output_file):
     """获取当前屏幕截图"""
@@ -15,6 +12,54 @@ def capture_screen(output_file):
         sct_img = sct.grab(monitor)
         # 将捕获的图像保存为文件
         mss.tools.to_png(sct_img.rgb, sct_img.size, output=output_file)
+
+class ImageSelector:
+    def __init__(self, image_path):
+        self.drawing = False  # 是否正在绘制矩形
+        self.ix, self.iy = -1, -1  # 矩形起始坐标
+        self.fx, self.fy = -1, -1  # 矩形结束坐标
+        self.img = cv2.imread(image_path)  # 读取图片
+        if self.img is None:
+            raise ValueError("图片路径错误或图片无法读取！")
+        self.center_x, self.center_y = -1, -1  # 矩形框中心点坐标，初始化为无效值
+
+    def draw_rectangle(self, event, x, y, flags, param):
+        if event == cv2.EVENT_LBUTTONDOWN:  # 鼠标左键按下
+            self.drawing = True
+            self.ix, self.iy = x, y  # 记录起始点
+
+        elif event == cv2.EVENT_MOUSEMOVE:  # 鼠标移动
+            if self.drawing:
+                img_copy = self.img.copy()
+                cv2.rectangle(img_copy, (self.ix, self.iy), (x, y), (0, 255, 0), 2)  # 实时绘制矩形
+                cv2.imshow("Image", img_copy)
+
+        elif event == cv2.EVENT_LBUTTONUP:  # 鼠标左键释放
+            self.drawing = False
+            self.fx, self.fy = x, y  # 记录结束点
+            cv2.rectangle(self.img, (self.ix, self.iy), (self.fx, self.fy), (0, 255, 0), 2)  # 绘制最终矩形
+            cv2.imshow("Image", self.img)
+
+            # 计算矩形框的中心点坐标
+            self.center_x = (self.ix + self.fx) // 2
+            self.center_y = (self.iy + self.fy) // 2
+
+    def get_center(self):
+        # 返回矩形框的中心点坐标
+        return self.center_x, self.center_y
+
+    def run(self):
+        # 创建窗口并最大化
+        cv2.namedWindow("Image", cv2.WINDOW_NORMAL)
+        cv2.setWindowProperty("Image", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+
+        # 设置鼠标回调函数
+        cv2.setMouseCallback("Image", self.draw_rectangle)
+
+        # 显示图片
+        cv2.imshow("Image", self.img)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
 
 def get_config_param(param_config_path = 'config/config.json'):
     """获取配置参数"""
@@ -27,20 +72,3 @@ def get_config_param(param_config_path = 'config/config.json'):
     return (botton_delay, botton_location,
             config['click_time_randomly_offset_maximum'],
             config['click_location_randomly_offset_maximum'])
-
-def mouse_click(param_config_path = 'config/config.json'):
-    """模拟鼠标点击"""
-    (botton_delay, botton_location, time_offset, location_offset) = get_config_param(param_config_path)
-
-    while True:
-        pyautogui.PAUSE = 0
-
-        if keyboard.is_pressed('esc'):
-            break
-
-        for i in range(len(botton_delay)):
-            click_x = botton_location[i][0] + random.randint(1, int(location_offset))
-            click_y = botton_location[i][1] + random.randint(1, int(location_offset))
-            click_delay = (botton_delay[i] + random.randint(1, int(time_offset)))/1000
-            pyautogui.click(click_x, click_y)
-            time.sleep(click_delay)
--- main.py
+++ main.py
@@ -1,25 +1,128 @@
+import re
 import time
+import random
+import pyautogui
+import keyboard
 import tkinter as tk
+from PIL import Image, ImageTk
 from Tools import *
 
-# 创建主窗口
-root = tk.Tk()
-# 设置窗口标题
-root.title("AUTOMATIC_CLICKER")
-# 设置窗口大小
-root.geometry("400x300")
+CLICK_BUTTON_NUMS = 8
+CONFIG_PATH = 'config/config.json'
+SCREENSHOT_PATH = 'tmp/screenshot.png'
 
-def on_button_click():
-    print("Button clicked!")
+with open(CONFIG_PATH, 'r') as f:
+    CONFIG = json.load(f)
+
+
+root = tk.Tk()
+root.title("AUTOMATIC_CLICKER -- designed by 孙笑川")
+root.geometry("600x600")
+# 显示sxc照片
+image = Image.open('config/sxc.jpg')
+image = image.resize((60, 80))
+photo = ImageTk.PhotoImage(image)
+label = tk.Label(root, image=photo)
+label.image = photo
+label.place(relx=0.1, rely=0.05, anchor=tk.CENTER)
+
+def save_changed_click_location_set(event):
+    """保存修改的点击位置"""
+    # capture_screen
     root.iconify()
     time.sleep(0.2)
     capture_screen(output_file = 'tmp/screenshot.png')
     root.deiconify()
+    # draw rect
+    selector = ImageSelector(SCREENSHOT_PATH)  # 替换为你的图片路径
+    selector.run()
+    center_x, center_y = selector.get_center()
+    # save changed click location set
+    index = int(re.search(r'\d+$', str(event.widget)).group()) if re.search(r'\d+$', str(event.widget)) else 1
+    CONFIG[str(index) + '_botton_location'] = [center_x, center_y]
 
+    with open(CONFIG_PATH, 'w', encoding='utf-8') as file:
+        json.dump(CONFIG, file)
 
-button = tk.Button(root, text="截图", command=on_button_click)
-button.pack()
+def save_changed_click_delay_set(event):
+    """保存修改的点击间隔"""
+    user_input = event.widget.get("1.0", tk.END).strip()
+    # save changed set
+    index = int(int(re.search(r'\d+$', str(event.widget)).group())/2)
+    CONFIG[str(index) + '_botton_delay'] = int(user_input) if user_input else "None"
+    if not user_input: 
+        CONFIG[str(index) + '_botton_location'] = "None"
 
+    with open(CONFIG_PATH, 'w', encoding='utf-8') as file:
+        json.dump(CONFIG, file)
+
+def save_changed_click_time_randomly_offset_maximum(event):
+    """修改点击时间随机偏移最大值"""
+    user_input = event.widget.get("1.0", tk.END).strip()
+    CONFIG['click_time_randomly_offset_maximum'] = int(user_input)
+
+    with open(CONFIG_PATH, 'w', encoding='utf-8') as file:
+        json.dump(CONFIG, file)
+
+def save_changed_click_location_randomly_offset_maximum(event):
+    """修改点击位置随机偏移最大值"""
+    user_input = event.widget.get("1.0", tk.END).strip()
+    CONFIG['click_location_randomly_offset_maximum'] = int(user_input)
+
+    with open(CONFIG_PATH, 'w', encoding='utf-8') as file:
+        json.dump(CONFIG, file)
+
+def mouse_click(param_config_path = 'config/config.json'):
+    """模拟鼠标点击"""
+    (botton_delay, botton_location, time_offset, location_offset) = get_config_param(param_config_path)
+    root.iconify()
+    time.sleep(0.2)
+    while True:
+        pyautogui.PAUSE = 0
+
+        if keyboard.is_pressed('esc'):
+            root.deiconify()
+            break
+
+        for i in range(min(len(botton_delay) , len(botton_location))):
+            click_x = botton_location[i][0] + random.randint(0, int(location_offset))
+            click_y = botton_location[i][1] + random.randint(0, int(location_offset))
+            click_delay = (botton_delay[i] + random.randint(0, int(time_offset)))/1000
+            pyautogui.click(click_x, click_y)
+            time.sleep(click_delay)
+
+# 参数设置控件
+tk.Label(root, text='按键坐标', bg='#D1EEEE').place(relx=0.35, rely=0.05, anchor=tk.CENTER, width=80, height=30)
+tk.Label(root, text='按键间隔', bg='#D1EEEE').place(relx=0.6, rely=0.05, anchor=tk.CENTER, width=80, height=30)
+for i in range(CLICK_BUTTON_NUMS):
+
+    button_set = tk.Button(root, text='按键' + str(i + 1) + '设置', bg='#D1EEEE')
+    button_set.place(relx=0.1, rely=0.15 + 0.08 * i, anchor=tk.CENTER, width=80, height=30)
+    button_set.bind("<Button-1>", save_changed_click_location_set)
+
+    rect_set = tk.Text(root, width=15, height=1)
+    rect_set.place(relx=0.35, rely=0.15 + 0.08 * i, anchor=tk.CENTER)
+    rect_set.insert(tk.END, str(CONFIG[str(i + 1) + '_botton_location']))
+
+    delay_set = tk.Text(root, width=10, height=1)
+    delay_set.place(relx=0.6, rely=0.15 + 0.08 * i, anchor=tk.CENTER)
+    delay_set.insert(tk.END, CONFIG[str(i + 1) + '_botton_delay'])
+    delay_set.bind("<FocusOut>", save_changed_click_delay_set)
+
+tk.Label(root, text='点击时间随机偏移', bg='#D1EEEE').place(relx=0.85, rely=0.15, anchor=tk.CENTER, width=150, height=30)
+time_offset = tk.Text(root, width=10, height=1)
+time_offset.place(relx=0.85, rely=0.23, anchor=tk.CENTER)
+time_offset.insert(tk.END, CONFIG['click_time_randomly_offset_maximum'])
+time_offset.bind("<FocusOut>", save_changed_click_time_randomly_offset_maximum)
+
+tk.Label(root, text='点击位置随机偏移', bg='#D1EEEE').place(relx=0.85, rely=0.35, anchor=tk.CENTER, width=150, height=30)
+time_offset = tk.Text(root, width=10, height=1)
+time_offset.place(relx=0.85, rely=0.43, anchor=tk.CENTER)
+time_offset.insert(tk.END, CONFIG['click_location_randomly_offset_maximum'])
+time_offset.bind("<FocusOut>", save_changed_click_location_randomly_offset_maximum)
+
+# 自动点击部分控件
+tk.Button(root, text="自动点击", bg = '#FFFF00', command=mouse_click).place(relx=0.5, rely=0.9, anchor=tk.CENTER, width=100, height=40)
 
 
 # 进入主事件循环
